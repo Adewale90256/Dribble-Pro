4a2aef4454f1a563390b50205ea61ea13478d145
Adeniji Adewale John <156160585+Adewale90256@users.noreply.github.com>
2025-01-30 12:24:36 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+"use strict";
+document.querySelector(".left").
+
+// const hoverOn = addEventListener("click", function () {
+//   leftEl.classList.add("underline");
+// });
+// console.log(hoverOn);
